8df0a2d629c3174d7f99f23faf3ef161e7e77e82
Adam Derewecki <adam@causes.com>
2012-04-24 10:27:12 -0700

    initial commit; 'rails new mynextmuni'

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mynextmuni::Application.config.session_store :cookie_store, :key => '_mynextmuni_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mynextmuni::Application.config.session_store :active_record_store
