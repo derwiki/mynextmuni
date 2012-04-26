4081d8629379d888732837bebd2b4d2caf4e3093
Adam Derewecki <adam@causes.com>
2012-04-26 09:58:50 -0700

    Switch from Rails to Sinatra

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mynextmuni::Application.config.secret_token = 'a404c78546c7e063c76c5c85a5c203b60579e0445630be8d77fd866ce5ef576eb8f7ed118a862a1f149fb1ca9a43d48047eb23686db50d2c44ca69bd2d407b17'
