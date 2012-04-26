c1ef9107bc66fbba21e7781764095bd3956c63ab
Adam Derewecki <adam@causes.com>
2012-04-26 10:47:02 -0700

    Add `Page rendered` info, make variables more JavaScripty

--- public/javascripts/mynextmuni.js
+++ public/javascripts/mynextmuni.js
@@ -5,10 +5,14 @@ $.urlParam = function(name, querystring){
     return results[1] || 0;
 }
 
-var routes;
 $(function() {
+  var d = new Date(),
+      seconds = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds(),
+      minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes(),
+      hours = d.getHours();
+  $('#pageRenderInfo span').text([hours, minutes, seconds].join(':'));
   var NEXTMUNI_API_BASE = 'http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&';
-  routes = {
+  var routes = {
     'Mission Cliffs to Work': [
       {route: 'r=27&d=27_IB1&s=3737&ts=3735', stop: '27 @ 19th/Bryant'},
       {route: 'r=12&d=12_IB1&s=4671&ts=4668', stop: '12 @ 18th/Folsom'},
@@ -31,18 +35,18 @@ $(function() {
     var locations = routes[key];
     var locHTML = $('<div><strong>' + key + '</strong></div>');
     $.each(locations, function(idx, loc) {
-      locHTML.append($('<div id="' + row_id(loc) + '">' + loc['stop'] + ': '));
+      locHTML.append($('<div id="' + rowId(loc) + '">' + loc['stop'] + ': '));
     });
     $('body').append(locHTML);
   }
 
-  function row_id(loc) { 
+  function rowId(route) {
 	return $.map(['r', 'd', 's', 'ts'], function(k) {
-	  return $.urlParam(k, '?' + loc['route']);
+	  return $.urlParam(k, '?' + route['route']);
     }).join("_");
   }
   
-  function seconds_to_display(seconds) {
+  function secondsToDisplay(seconds) {
     var minutes = Math.floor(seconds / 60);
     var seconds = seconds % 60;
     return ["<span class='time'><span class='minutes'>", minutes,
@@ -58,10 +62,10 @@ $(function() {
       'success': function(xml) {
         var predictions = $(xml).find('prediction').slice(0, 2);
         var formatted_predictions = $.map(predictions, function(p) {
-          return seconds_to_display($(p).attr('seconds'));
+          return secondsToDisplay($(p).attr('seconds'));
         });
         route['prediction'] = formatted_predictions.join(', ');
-        $('#' + row_id(route)).html(route['stop'] + ': ' + route['prediction']);
+        $('#' + rowId(route)).html(route['stop'] + ': ' + route['prediction']);
 
         var randomDelay = Math.floor(Math.random()*3) * 1000; // msec
         setInterval(function() { refreshPrediction(route); }, randomDelay);
